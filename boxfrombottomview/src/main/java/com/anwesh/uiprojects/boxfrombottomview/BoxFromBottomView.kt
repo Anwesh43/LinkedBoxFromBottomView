0e7705144941540f24e6fc0dcedff4e08578409f
Anwesh43 <anweshthecool0@gmail.com>
2020-10-16 02:22:28 +0530

    adding BoxFromBottomView class

--- boxfrombottomview/src/main/java/com/anwesh/uiprojects/boxfrombottomview/BoxFromBottomView.kt
+++ boxfrombottomview/src/main/java/com/anwesh/uiprojects/boxfrombottomview/BoxFromBottomView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawBFBNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBoxFromBottom(scale, w, h, paint)
 }
+
+class BoxFromBottomView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action){
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
