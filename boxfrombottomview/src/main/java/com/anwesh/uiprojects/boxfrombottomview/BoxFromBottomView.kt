6f7b71cc7ae0d291fce6497968b91d41dc315368
Anwesh43 <anweshthecool0@gmail.com>
2020-10-16 02:29:56 +0530

    adding BFBNode class

--- boxfrombottomview/src/main/java/com/anwesh/uiprojects/boxfrombottomview/BoxFromBottomView.kt
+++ boxfrombottomview/src/main/java/com/anwesh/uiprojects/boxfrombottomview/BoxFromBottomView.kt
@@ -129,4 +129,45 @@ class BoxFromBottomView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BFBNode(var i : Int, val state : State = State()) {
+
+        private var next : BFBNode? = null
+        private var prev : BFBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BFBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBFBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BFBNode {
+            var curr : BFBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
