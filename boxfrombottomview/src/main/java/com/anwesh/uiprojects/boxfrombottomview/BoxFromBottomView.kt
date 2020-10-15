45b596d7bc8b1a8ad74dd76ef02b730ecd71ed78
Anwesh43 <anweshthecool0@gmail.com>
2020-10-16 02:31:44 +0530

    adding BoxFromBottom class

--- boxfrombottomview/src/main/java/com/anwesh/uiprojects/boxfrombottomview/BoxFromBottomView.kt
+++ boxfrombottomview/src/main/java/com/anwesh/uiprojects/boxfrombottomview/BoxFromBottomView.kt
@@ -170,4 +170,27 @@ class BoxFromBottomView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BoxFromBottom(var i : Int) {
+
+        private var curr : BFBNode = BFBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
